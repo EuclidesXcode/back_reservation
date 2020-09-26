b660b7bfa1c31cff379d432917f57d84287fee5a
euclides <euclides.nascimento@vivitech.com>
2020-09-26 00:42:34 -0300

    update 3

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(routers);
-const port = process.env.PORT || 2000;
+const port = process.env.PORT || 80;
 
 app.listen( port, () => {
     console.log(`server on ${port}`)
