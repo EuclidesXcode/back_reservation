53cfc35836601c4b923adc040e5bbdccfe9aef2f
euclides <euclides.nascimento@vivitech.com>
2020-10-04 00:36:11 -0300

    update 44

--- routers/schedule.js
+++ routers/schedule.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const routers = express.Router();
-const shedueService = require('../services/schedule')
+const sheduleService = require('../services/schedule')
 
 routers.post('/get', (req,res) => {
-    shedueService.getAll(req,res)
+    sheduleService.getAll(req,res)
     
 });
 routers.post('/filter', (req,res) => {
-    shedueService.filter(req,res)
+    sheduleService.filter(req,res)
 });
 
 routers.post('/', (req,res) => {
-    shedueService.save(req,res)
+    sheduleService.save(req,res)
 });
 routers.delete('/:id', (req,res) => {
-    shedueService.delete(req,res)
+    sheduleService.delete(req,res)
 });
 routers.put('/:id', (req,res) => {
-    shedueService.update(req,res)
+    sheduleService.update(req,res)
 });
 
 module.exports = routers;
